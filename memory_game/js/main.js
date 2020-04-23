216f1226c891af915c2263ca269ca3b064f00558
Fletcher Jones <fletcherejones@gmail.com>
2020-04-22 22:46:15 -0400

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-let card1 = "queen";
-let card2 = "king";
-let card3 = "king";
-let card4 = "queen";
+let cards = ["queen", "king", "queen", "king"];
+let cardsInPlay = [];
 
-console.log("You flipped" + card1);
-console.log("You flipped" + card4);
+let card1 = cards[0];
+let card2 = cards[2];
+
+cardsInPlay.push(card1);
+console.log("You flipped a " + cardsInPlay[0]);
+
+cardsInPlay.push(card2)
+console.log("You flipped a " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	};
+};
